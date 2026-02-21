9492f277b6cddac7a5f9480e63f48cc0e6e2f118
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-02-21 16:20:36 +0530

    [Ashish Dubey] Completes UC1

--- QuantityMeasurementApp/Menu.cs
+++ QuantityMeasurementApp/Menu.cs
@@ -14,14 +14,19 @@ namespace QuantityMeasurementApp
                 while (!int.TryParse(Console.ReadLine(), out input)) Console.WriteLine("Invalid Input : ");
                 if (input == 0) return;
                 if (input == 1)
-                {
+                {// creating object for FeetUtility class
                     IFeet obj = new FeetUtility();
-                    double Feet_1;
-                    double Feet_2;
+                    double Feet_1;// input variable for first value
+                    double Feet_2;// input variable for second value
                     Console.Write("Enter first value : ");
-                    while (!double.TryParse(Console.ReadLine(), out Feet_1)) Console.WriteLine("Invalid Input : ");
+                    while (!double.TryParse(Console.ReadLine(), out Feet_1)){
+                        Console.WriteLine("Invalid Input : ");// validating input for first value
+                    }
                     Console.Write("Enter Second value : ");
-                    while (!double.TryParse(Console.ReadLine(), out Feet_2)) Console.WriteLine("Invalid Input : ");
+                    while (!double.TryParse(Console.ReadLine(), out Feet_2))
+                    {
+                        Console.WriteLine("Invalid Input : ");// validating input for second value
+                    } 
                     obj.CompareFeet(Feet_1, Feet_2);
                 }
             } while (true);
