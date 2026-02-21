cc1c8c62876218d0b8b9d639c229177dadf7155d
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-02-21 15:58:59 +0530

    [Ashish Dubey] Completes UC1

--- QuantityMeasurementApp/Menu.cs
+++ QuantityMeasurementApp/Menu.cs
@@ -11,14 +11,20 @@ namespace QuantityMeasurementApp
             {
                 Console.WriteLine("\nPress 0. To Exit");
                 Console.WriteLine("Press 1. For checking Feet Equality");
-                while(!int.TryParse(Console.ReadLine(),out input))Console.WriteLine("Invalid Input : ");
-                if(input == 0)return;
+                while (!int.TryParse(Console.ReadLine(), out input)) Console.WriteLine("Invalid Input : ");
+                if (input == 0) return;
                 if (input == 1)
                 {
                     IFeet obj = new FeetUtility();
-                    obj.CompareFeet();
+                    double Feet_1;
+                    double Feet_2;
+                    Console.Write("Enter first value : ");
+                    while (!double.TryParse(Console.ReadLine(), out Feet_1)) Console.WriteLine("Invalid Input : ");
+                    Console.Write("Enter Second value : ");
+                    while (!double.TryParse(Console.ReadLine(), out Feet_2)) Console.WriteLine("Invalid Input : ");
+                    obj.CompareFeet(Feet_1, Feet_2);
                 }
-            }while(true);
+            } while (true);
         }
     }
 }
