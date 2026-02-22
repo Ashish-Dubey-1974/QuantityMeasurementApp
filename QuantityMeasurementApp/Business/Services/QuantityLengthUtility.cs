4d36af3e45fa9015e3995cb9919de40bf553e3f2
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-02-22 11:05:28 +0530

    [Ashish Dubey] Add features for UC3

--- QuantityMeasurementApp/Business/Services/QuantityLengthUtility.cs
+++ QuantityMeasurementApp/Business/Services/QuantityLengthUtility.cs
@@ -2,26 +2,14 @@ using QuantityMeasurementApp.Business.Interfaces;
 using QuantityMeasurementApp.Model;
 namespace QuantityMeasurementApp.Business.Services
 {
-    public class QuantityLengthUtility : IQuantityLength
+    public class QuantityLengthUtility: IQuantityLength
     {
-        public bool CheckEquality(double value1, LengthUnit unit1, double value2, LengthUnit unit2)
+        public bool CheckEquality(double value1, LengthUnit unit1,double value2, LengthUnit unit2)
         {
-            double valueInInches1 = ConvertToInches(value1, unit1);
-            double valueInInches2 = ConvertToInches(value2, unit2);
-            return valueInInches1 == valueInInches2;
-        }
+            QuantityLength q1 = new QuantityLength(value1, unit1);
+            QuantityLength q2 = new QuantityLength(value2, unit2);
 
-        private double ConvertToInches(double value, LengthUnit unit)
-        {
-            switch (unit)
-            {
-                case LengthUnit.Feet:
-                    return value * 12; // 1 foot = 12 inches
-                case LengthUnit.Inch:
-                    return value;
-                default:
-                    throw new ArgumentException("Invalid length unit");
-            }
+            return q1.Equals(q2);
         }
     }
 }
